bacd59bb59fa99d8d9ca09c7d034a67dfae36308
Yasmim Silva <yasmim.design@hotmail.com>
2023-06-15 18:50:38 +0100

    Coloquei o counter da pagina inicial para funciona pra frente e pra tras

--- js/slider.js
+++ js/slider.js
@@ -4,7 +4,15 @@
 var sliderContainer = document.querySelector('.ys-slider-container');
 var sliderList = document.querySelector('.ys-slider-list');
 var sliderItem = document.querySelectorAll('.ys-slider-item');
+const sliderTotalItems = sliderItem.length;
 var sliderListWidth = null;
+var prevItem = document.querySelector('.ys-item-prev');
+var nextItem = document.querySelector('.ys-item-next');
+var sliderPos = 0;
+var currentSlide = document.querySelector('.ys-current-slide');
+var totalSlide = document.querySelector('.ys-total-slide');
+var currentCounter = 1;
+
 
 //Capturando larguras individuais
 var containerWidth = sliderContainer.parentElement.offsetWidth;
@@ -26,9 +34,7 @@ sliderList.style.width = sliderListWidth + 'px'
 
 //Fazendo Animações do Slider onClick
 
-var prevItem = document.querySelector('.ys-item-prev');
-var nextItem = document.querySelector('.ys-item-next');
-var sliderPos = 0;
+
 
 //HANDLERS
 
@@ -61,12 +67,45 @@ var prevSlideAnim = function () {
     });
 }
 
+//Counter Formater
+var counterFormatter = function (n) {
+    if (n < 10) {
+        return '0' + n
+    } else {
+        return n;
+    }
+}
+
+//Counter Add
+
+var counterAdd = function () {
+    if ((currentCounter >= 0) && (currentCounter < sliderTotalItems)) {
+        currentCounter++;
+        currentSlide.innerHTML = counterFormatter(currentCounter);
+    }
+}
+
+//Counter Remove
+
+var counterRemove = function () {
+    if ((currentCounter > 1) && (currentCounter <= sliderTotalItems)) {
+        currentCounter--;
+        currentSlide.innerHTML = counterFormatter(currentCounter);
+    }
+}
+
+//ACTIONS
+
+totalSlide.innerHTML = counterFormatter(sliderTotalItems);
+
+
 nextItem.addEventListener('click', function () {
     nextSlideAnim();
+    counterAdd();
 
 });
 
 prevItem.addEventListener('click', function () {
     prevSlideAnim();
-
+    counterRemove();
 });
